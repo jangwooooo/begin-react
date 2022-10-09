efbc5a04a3db1a90810ae04f19827f49baa59dc2
jangwooooo <s22018@gsm.hs.kr>
2022-10-09 18:31:54 +0900

    6.0 Introduction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const onClick = () => setCount((current) => current + 1);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <h1>{count}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
